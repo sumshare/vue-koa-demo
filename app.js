0384f63ec2b45755dbaa3c7a1804fa0dce28644a
sumshare <sumshare@126.com>
2017-11-24 09:22:00 +0800

    testVSGIT

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// "server": "nodemon --delay 2000ms -w app.js -w server server-entry.js"
+// 真正入口是server-entry.js
 import Koa from 'koa';
 import json from 'koa-json';
 import logger from 'koa-logger';
@@ -12,49 +14,55 @@ import koa_bodyparser from "koa-bodyparser";
 
 const app = new Koa();
 const router = koa_router();
-
+// 挂载各种中间件
 app.use(koa_bodyparser());
 app.use(json());
 app.use(logger());
 
-app.use(async function(ctx, next){
-  let start = new Date;
-  await next();
-  let ms = new Date - start;
-  console.log('%s %s - %s', ctx.method, ctx.url, ms);
-});
-
-app.use(async function(ctx, next){  //  如果JWT验证失败，返回验证失败信息
-  try {
+app.use(async function(ctx, next) {
+    let start = new Date;
     await next();
-  } catch (err) {
-    if (401 === err.status) {
-      ctx.status = 401;
-      ctx.body = {
-        success: false,
-        token: null,
-        info: 'Protected resource, use Authorization header to get access'
-      };
-    } else {
-      throw err;
+    let ms = new Date - start;
+    console.log('%s %s - %s', ctx.method, ctx.url, ms);
+});
+
+//  如果JWT验证失败，返回验证失败信息
+app.use(async function(ctx, next) { 
+    try {
+        await next();
+    } catch (err) {
+        if (401 === err.status) {
+            ctx.status = 401;
+            ctx.body = {
+                success: false,
+                token: null,
+                info: 'Protected resource, use Authorization header to get access'
+            };
+        } else {
+            throw err;
+        }
     }
-  }
 });
 
-app.on('error', function(err, ctx){
-  console.log('server error', err);
+app.on('error', function(err, ctx) {
+    console.log('server error', err);
 });
+// 挂载到koa-router上，同时会让所有的auth的请求路径前面加上'/auth'的请求路径。
+router.use('/auth', auth.routes());
+// 所有走/api/打头的请求都需要经过jwt验证。
+router.use("/api", jwt({
+    secret: 'vue-koa-demo'
+}), api.routes());
 
-
-router.use('/auth', auth.routes()); // 挂载到koa-router上，同时会让所有的auth的请求路径前面加上'/auth'的请求路径。
-router.use("/api",jwt({secret: 'vue-koa-demo'}),api.routes()) // 所有走/api/打头的请求都需要经过jwt验证。
-
-app.use(router.routes()); // 将路由规则挂载到Koa上。
+// 将路由规则挂载到Koa上。
+app.use(router.routes());
 app.use(historyApiFallback());
-app.use(serve(path.resolve('dist'))); // 将webpack打包好的项目目录作为Koa静态文件服务的目录
 
-app.listen(8889,() => {
-  console.log('Koa is listening in 8889');
+// 将webpack打包好的项目目录作为Koa静态文件服务的目录
+app.use(serve(path.resolve('dist')));
+
+app.listen(8889, () => {
+    console.log('Koa is listening in 8889');
 });
 
 export default app;
--- server-entry.js
+++ server-entry.js
@@ -1,10 +1,23 @@
+// 通过babel-register在node中使用es6
+// babel依赖
+// {  
+//   "dependencies": {  
+//     "babel": "^6.23.0",  
+//     "babel-core": "^6.24.1",  
+//     "babel-preset-es2015": "^6.24.1",  
+//     "babel-preset-react": "^6.24.1",  
+//     "babel-preset-stage-1": "^6.24.1",  
+//     "babel-register": "^6.24.1"  
+//   }  
+// }  
 require("babel-core/register")({
-  "presets": [
-    ["env", {
-      "targets": {
-        "node": true
-      }
-    }]
-  ]
+    "presets": [
+        ["env", {
+            "targets": {
+                "node": true
+            }
+        }]
+    ]
 });
+// 启用app
 require("./app.js")
